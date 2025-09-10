2467a9dc9e16ac98377afee09325864da400b503
noleynik29 <noleynik29@gmail.com>
2025-09-10 15:25:57 +0300

    Filtered out only the words starting with w, and removed any punctuation. The result returns as a naturally sorted lowercase array.

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,50 @@
 package core.basesyntax;
 
+import java.io.*;
+import java.util.Arrays;
+
 public class FileWork {
     public String[] readFromFile(String fileName) {
         //write your code here
-        return null;
+        String[] result = null;
+        int wordCount = 0;
+        int arrayCount = 0;
+        File file = new File(fileName);
+        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+            String line;
+            String[] words;
+            while ((line = reader.readLine()) != null) {
+                words = line.split("\\s+");
+                for (String word : words) {
+                    if (word.startsWith("w") || word.startsWith("W")) {
+                        wordCount++;
+                    }
+                }
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException("File not found: " + e);
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read file: " + e);
+        }
+        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+            String line;
+            String[] words;
+            result = new String[wordCount];
+            while ((line = reader.readLine()) != null) {
+                words = line.split("\\s+");
+                for (String word : words) {
+                    if (word.startsWith("w") || word.startsWith("W")) {
+                        result[arrayCount] = word.replaceAll("\\p{Punct}", "").toLowerCase();
+                        arrayCount++;
+                    }
+                }
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException("File not found: " + e);
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read file: " + e);
+        }
+        Arrays.sort(result);
+        return result;
     }
 }
